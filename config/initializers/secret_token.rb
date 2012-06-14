3a2030e19100d905b747da4eb38d19064c038c71
Alex Mejía <maxelmejia@hotmail.com>
2012-06-13 23:03:16 -0600

    no autentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = '4b0b793179acfbafdfe89e894db79b3ac7d18d1bebff7e8db803a05c24261a846f9aaacfc95c6bd18b2147413abba24411e5545c168ae42db166179cbd351067'
