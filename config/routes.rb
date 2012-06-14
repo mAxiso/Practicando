876882d1ee0100d20da22d3a4f28b9a707c625fb
Alex Mejía <maxelmejia@hotmail.com>
2012-06-13 23:55:50 -0600

    user model created

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Tasks::Application.routes.draw do
 
 
  
+  resources :users
+
+  root to: "home#index"
   resources :tasks
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120614013258) do
+ActiveRecord::Schema.define(:version => 20120614055428) do
 
   create_table "tasks", :force => true do |t|
     t.string   "description"
     t.integer  "priority"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "username"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
